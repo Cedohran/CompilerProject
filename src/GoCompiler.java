1b1823dfdebb98575c23fb62d9347634e54a3ef2
Cedohran <cedohran@gmx.de>
2022-12-11 15:35:23 +0100

    schick jemacht

--- src/GoCompiler.java
+++ src/GoCompiler.java
@@ -9,38 +9,49 @@ import java.io.InputStream;
 
 public class GoCompiler {
     public static void main(String[] args) throws IOException {
-        InputStream input = new FileInputStream("visit.txt");
+        if(args.length == 0) {
+            System.err.println("Argument for file missing.");
+            System.exit(1);
+        }
+        InputStream input = new FileInputStream(args[0]);
 
+        //lexing
         GoLexer lexer = new GoLexer(CharStreams.fromStream(input));
         CommonTokenStream tokens = new CommonTokenStream( lexer );
 
+        //parsing
         GoParser parser = new GoParser( tokens );
         ParseTree tree = parser.program();
         ParseTreeWalker walker = new ParseTreeWalker();
 
-
+        //AST creation
         GoVisitorAstCreator astCreator =  new GoVisitorAstCreator();
-        walker.walk(astCreator, tree);
-        System.out.println(astCreator.AST.toString());
+        try {
+            walker.walk(astCreator, tree);
+        } catch (Exception e) {
+            System.err.println("Parsing failed.");
+            System.exit(1);
+        }
+        System.out.println("Parsing success.");
 
+        //symbol table creation
         SymbolTableCreator symbolTableCreator = new SymbolTableCreator();
         walker.walk(symbolTableCreator, tree);
 
-        boolean typeError = false;
+        //typechecking
         GoTypeChecker typeChecker = new GoTypeChecker(symbolTableCreator);
         try {
             typeChecker.check(astCreator.AST);
         } catch (TypeCheckException e) {
             System.err.println(e.getMessage());
-            typeError = true;
+            System.exit(1);
         }
+        System.out.println("Typechecking success.");
 
-        if(!typeError) System.out.println("TypeChecking success.");
-
-        //System.out.println(parser.creator.programTree.toString());
-
-//        visitor.varMap.forEach((k, v) -> {
-//            System.out.println("ID: "+k+" Type: "+v.getName());
-//        });
+        //optional AST to std.out
+        if(args.length > 1 && args[1].equals("ast")) {
+            System.out.println("\nAbstract Syntax Tree :\n");
+            System.out.println(astCreator.AST.toString());
+        }
     }
 }
